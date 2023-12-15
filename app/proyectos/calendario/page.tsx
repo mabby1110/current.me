56e9481a52faf34c76ae411fe59d855412be2ffc
mabby <rznns.mabby@gmail.com>
2023-12-15 10:08:32 -0600

    calendario, datos para calendario ok

--- app/proyectos/calendario/page.tsx
+++ app/proyectos/calendario/page.tsx
@@ -1,13 +1,21 @@
-import getMonth from "@/app/lib/util"
+"use client"
+import getMonth from "@/app/proyectos/calendario/util"
 
 export default function VistaCalendario() {
     const monthMatrix = getMonth()
 
-    console.table(typeof getMonth())
-
     return (
         <div>
             <h1>calendario :)</h1>
+            <>
+                <div>
+                    <h1>calendar header</h1>
+                    <div>
+                        <p>sidebar</p>
+                        
+                    </div>
+                </div>
+            </>
         </div>
     )
 }
