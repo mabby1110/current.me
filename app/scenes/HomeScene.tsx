6b6951714821055088cd3d0b563892b61f0a0b28
mabby <rznns.mabby@gmail.com>
2024-01-18 18:45:40 -0600

    minor fix

--- app/scenes/HomeScene.tsx
+++ app/scenes/HomeScene.tsx
@@ -1,59 +1,64 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import * as THREE from 'three'
-import Style from '@/app/scenes/homeScene.module.css'
 
 export default function HomeScene() {
-  
-  if (typeof window !== 'undefined') {
-    const width = window.innerWidth
-    const height = window.innerHeight
-    
-    const renderer = new THREE.WebGLRenderer({
-      canvas: document.getElementById('app') as HTMLCanvasElement
-    })
-    renderer.setSize(width, height)
-    const mainCamera = new THREE.PerspectiveCamera(60, width/height, 0.1, 100)
-    
-    const scene = new THREE.Scene()
-    
-    const geometry = new THREE.BoxGeometry()
-    const material = new THREE.MeshPhongMaterial({color: 0xFFFFFF})
-    const cube = new THREE.Mesh(geometry, material)
-    cube.position.z = -5
-    cube.position.y = 1
-    scene.add(cube)
+  const [renderer, setRenderer] = useState(null)
+  const [mainCamera, setMainCamera] = useState(null)
+  const [scene, setScene] = useState(null)
 
-    const light = new THREE.DirectionalLight(0xFFFFFF, 1)
-    light.position.set(0, 4, 2)
-    scene.add(light)
-    
-    const orbitControl = new THREE.CameraHelper(mainCamera)
-    scene.add(orbitControl)
-    
-    renderer.render(scene, mainCamera)
+  useEffect(() => {
+    // if (typeof window !== 'undefined') {
+      const width = window.innerWidth
+      const height = window.innerHeight
 
-    useEffect(() => {
+      const renderer = new THREE.WebGLRenderer({ canvas: document.getElementById('app') as HTMLCanvasElement })
+      renderer.setSize(width, height)
+      const mainCamera = new THREE.PerspectiveCamera(60, width/height, 0.1, 100)
+      const scene = new THREE.Scene()
+
+      const geometry = new THREE.BoxGeometry()
+      const material = new THREE.MeshPhongMaterial({color: 0xFFFFFF})
+      const cube = new THREE.Mesh(geometry, material)
+      cube.position.z = -5
+      cube.position.y = 1
+      scene.add(cube)
+
+      const light = new THREE.DirectionalLight(0xFFFFFF, 1)
+      light.position.set(0, 6, 2)
+      scene.add(light)
+
+      const orbitControl = new THREE.CameraHelper(mainCamera)
+      scene.add(orbitControl)
+
+      setRenderer(renderer)
+      setMainCamera(mainCamera)
+      setScene(scene)
+
+      renderer.render(scene, mainCamera)
+    // }
+  }, [])
+
+  useEffect(() => {
+    if (renderer && mainCamera) {
       const handleResize = () => {
         const width = window.innerWidth
         const height = window.innerHeight
         renderer.setSize(width, height)
         renderer.render(scene, mainCamera)
       }
-      
+
       window.addEventListener('resize', handleResize)
-      
+
       // Set initial size
       handleResize()
-      
+
       return () => {
         window.removeEventListener('resize', handleResize)
       }
-    }, [])
-  }
+    }
+  }, [renderer, mainCamera])
 
   return (
     <canvas id="app"></canvas>
   )
 }
-
-
