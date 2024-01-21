ce87633769127d2e6068e930a4cf5c90771c56d8
mabby <rznns.mabby@gmail.com>
2024-01-20 18:49:08 -0600

    Tienda con grid y flexbox

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,8 @@
-"use client"
 import HomeScene from "./scenes/HomeScene"
 
 export default function Home() {
     return (
         <>
-
             <h1>
                 Hola, soy mabby
             </h1>
--- app/proyectos/tienda/page.tsx
+++ app/proyectos/tienda/page.tsx
@@ -5,40 +5,42 @@ export default function Tienda() {
   return (
     <div className={Style.mainContent}>
         <div className={Style.secHero}>hero</div>
-        <div className={Style.secSpot}>
-          <div className={Style.contenidoSpot}>Promos</div>
-        </div>
-        <div className={Style.secCarrito}>
-          <div className={Style.contenidoCarrito}>carrito</div>
-        </div>
-        <div className={Style.secCatalogo}>
-          <p className={Style.producto}>dd</p>
-          <p className={Style.producto}>dd</p>
-          <p className={Style.producto}>dd</p>
-          <p className={Style.producto}>dd</p>
-          <p className={Style.producto}>dd</p>
-          <p className={Style.producto}>dd</p>
-          <p className={Style.producto}>dd</p>
-          <p className={Style.producto}>dd</p>
-          <p className={Style.producto}>dd</p>
-          <p className={Style.producto}>dd</p>
-          <p className={Style.producto}>dd</p>
-          <p className={Style.producto}>dd</p>
-          <p className={Style.producto}>dd</p>
-          <p className={Style.producto}>dd</p>
-          <p className={Style.producto}>dd</p>
-          <p className={Style.producto}>dd</p>
-          <p className={Style.producto}>dd</p>
-          <p className={Style.producto}>dd</p>
-          <p className={Style.producto}>dd</p>
-          <p className={Style.producto}>dd</p>
-          <p className={Style.producto}>dd</p>
-          <p className={Style.producto}>dd</p>
-          <p className={Style.producto}>dd</p>
-          <p className={Style.producto}>dd</p>
-          <p className={Style.producto}>dd</p>
-          <p className={Style.producto}>dd</p>
-          <p className={Style.producto}>dd</p>
+        <div className={Style.secTienda}>
+          <div className={Style.secPromo}>
+            <div className={Style.contenidoPromo}>Promos</div>
+          </div>
+          <div className={Style.secCarrito}>
+            <div className={Style.contenidoCarrito}>carrito</div>
+          </div>
+          <div className={Style.secCatalogo}>
+            <p className={Style.producto}>dd</p>
+            <p className={Style.producto}>dd</p>
+            <p className={Style.producto}>dd</p>
+            <p className={Style.producto}>dd</p>
+            <p className={Style.producto}>dd</p>
+            <p className={Style.producto}>dd</p>
+            <p className={Style.producto}>dd</p>
+            <p className={Style.producto}>dd</p>
+            <p className={Style.producto}>dd</p>
+            <p className={Style.producto}>dd</p>
+            <p className={Style.producto}>dd</p>
+            <p className={Style.producto}>dd</p>
+            <p className={Style.producto}>dd</p>
+            <p className={Style.producto}>dd</p>
+            <p className={Style.producto}>dd</p>
+            <p className={Style.producto}>dd</p>
+            <p className={Style.producto}>dd</p>
+            <p className={Style.producto}>dd</p>
+            <p className={Style.producto}>dd</p>
+            <p className={Style.producto}>dd</p>
+            <p className={Style.producto}>dd</p>
+            <p className={Style.producto}>dd</p>
+            <p className={Style.producto}>dd</p>
+            <p className={Style.producto}>dd</p>
+            <p className={Style.producto}>dd</p>
+            <p className={Style.producto}>dd</p>
+            <p className={Style.producto}>dd</p>
+          </div>
         </div>
     </div>
   )
